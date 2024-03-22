25a41ea603a646e652bba1a62bcc7abbe95ea151
Andrea Hohnsen <a.hohnsen@mail.de>
2024-03-22 11:25:11 +0100

    Create character card

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+import createCharacterCard from "./components/CharacterCard/CharacterCard.js";
+
+const character = {
+  image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+  name: "Rick Sanchez",
+  status: "Alive",
+  type: "",
+  episode: "51",
+};
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
-const searchBarContainer = document.querySelector(
-  '[data-js="search-bar-container"]'
-);
+const searchBarContainer = document.querySelector('[data-js="search-bar-container"]');
 const searchBar = document.querySelector('[data-js="search-bar"]');
 const navigation = document.querySelector('[data-js="navigation"]');
 const prevButton = document.querySelector('[data-js="button-prev"]');
@@ -12,3 +20,7 @@ const pagination = document.querySelector('[data-js="pagination"]');
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+const card = createCharacterCard(character);
+
+cardContainer.append(card);
